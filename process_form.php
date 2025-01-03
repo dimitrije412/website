bdd456b567897766f8535979dbcc3cf962fd4264
Dimitrije Matijasevic <146454397+dimitrije412@users.noreply.github.com>
2025-01-03 20:38:17 +0100

    Add files via upload

---
+++ process_form.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "dimitrije.matijasevic@gmail.com";
+    $subject = "New message from your website";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+}
+?>
